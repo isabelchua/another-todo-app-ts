04adab66098905893570dc43b670cf2865920086
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-09-15 11:02:32 -0700

    finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import React from "react";
 import { NewNoteInput } from "./NewNoteInput";
+import { useSelector, useDispatch } from "react-redux";
+import { NotesState } from "./notesReducer";
 
 import "./App.css";
+import { addNote } from "./actions";
 
 function App() {
+	const notes = useSelector<NotesState, NotesState["notes"]>(
+		state => state.notes
+	);
+
+	const dispatch = useDispatch();
+
+	const onAddNote = (note: string) => {
+		dispatch(addNote(note));
+	};
+
 	return (
 		<>
-			<NewNoteInput addNote={alert} />
+			<NewNoteInput addNote={onAddNote} />
 			<ul>
-				<li>some note</li>
+				{notes.map(note => {
+					return <li key={note}>{note}</li>;
+				})}
 			</ul>
 		</>
 	);
